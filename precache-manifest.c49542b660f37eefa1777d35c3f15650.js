04cc1943cebd7eda699513ec28d2e382417d1405
Aditya2 Pawar <Aditya2.Pawar@ril.com>
2019-05-11 17:02:35 +0530

    Updates

---
+++ precache-manifest.c49542b660f37eefa1777d35c3f15650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3f819d390786f73416795d6408ee84",
+    "url": "/coffeecounter/index.html"
+  },
+  {
+    "revision": "13adb553d4d11d843095",
+    "url": "/coffeecounter/static/css/main.bb22621b.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/coffeecounter/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "13adb553d4d11d843095",
+    "url": "/coffeecounter/static/js/main.bde00750.chunk.js"
+  },
+  {
+    "revision": "16712b4f8b4da9e3aa3f",
+    "url": "/coffeecounter/static/js/runtime~main.947fe2b2.js"
+  },
+  {
+    "revision": "64e2d61c411eb72022006ace79a69ac5",
+    "url": "/coffeecounter/static/media/coffee-mug.64e2d61c.png"
+  }
+]);
